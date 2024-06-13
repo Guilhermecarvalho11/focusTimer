b3164ea8e5483098b89ffec9b1fc5d5d0be14141
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-06-13 17:38:11 -0300

    finish adjusting minutes and seconds

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,5 +1,9 @@
 import { controls } from "./elements.js";
 import * as actions from './actions.js';
+import * as el from './elements.js';
+import state from "./state.js";
+import { updateDisplay } from "./timer.js";
+
 
 export function registerConstrols() {
     controls.addEventListener('click', (event) => {
@@ -11,4 +15,23 @@ export function registerConstrols() {
 
         actions[act]();
     })
+}
+
+export function setMinutes(){
+el.minutes.addEventListener('focus', () => {
+    el.minutes.textContent = '';
+});
+
+el.minutes.onkeypress = (event) => /\d/.test(event.key);
+
+el.minutes.addEventListener('blur', (event) => {
+    let time = event.currentTarget.textContent; 
+    time = time > 60 ? 60 : time;
+
+    state.minutes = time;
+    state.seconds = 0;
+
+    updateDisplay();
+    el.minutes.removeAttribute('contenteditable');
+})
 }
